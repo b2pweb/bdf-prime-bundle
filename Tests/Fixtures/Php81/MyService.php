658b736736ad6ec00581b46f963e8fcf8ceeaef2
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2025-04-28 14:34:39 +0200

    ci: fix test on PHP < 8.1

--- Tests/Fixtures/Php81/MyService.php
+++ Tests/Fixtures/Php81/MyService.php
@@ -6,18 +6,23 @@ use Bdf\Prime\Repository\EntityRepository;
 use Bdf\PrimeBundle\Attribute\Repository;
 use Bdf\PrimeBundle\Tests\Fixtures\TestEntity;
 
-class MyService
-{
-    /**
-     * @var EntityRepository<TestEntity>
-     */
-    #[Repository(TestEntity::class)]
-    public $repository;
-
-    public function __construct(
+if (PHP_VERSION_ID >= 80100) {
+    class MyService
+    {
+        /**
+         * @var EntityRepository<TestEntity>
+         */
         #[Repository(TestEntity::class)]
-        EntityRepository $repository
-    ) {
-        $this->repository = $repository;
+        public $repository;
+
+        public function __construct(
+            #[Repository(TestEntity::class)]
+            EntityRepository $repository
+        ) {
+            $this->repository = $repository;
+        }
     }
+} else {
+    // Add class to avoid error in PHP 7.X
+    class MyService {}
 }
